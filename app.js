ffb3a298cd959bacaedc79b1068e2169506bfa78
Scott Wu <scott.wu.hjx@gmail.com>
2016-09-07 18:49:53 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+var express = require('express'),
+  config = require('./config/config');
+
+var app = express();
+
+require('./config/express')(app, config);
+
+app.listen(config.port, "0.0.0.0", function () {
+  console.log('Express server listening on port ' + config.port);
+});
+
